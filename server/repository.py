56144b4ad1ae7f1884d365815cd418586b4b7569
im-varun <vrun.mulchandani@gmail.com>
2026-02-16 17:42:15 -0600

    Add update and delete methods in repositories

--- server/repository.py
+++ server/repository.py
@@ -7,6 +7,14 @@ class ProjectRepository:
     def __init__(self):
         self.ref = firebase_db.reference("projects")
 
+    def create(self, project: Project):
+        project_id = str(uuid.uuid4())
+        project.id = project_id
+
+        self.ref.child(project_id).set(project.model_dump())
+
+        return project
+
     def get_all(self):
         data = self.ref.get()
         if not data or not isinstance(data, dict):
@@ -21,17 +29,25 @@ class ProjectRepository:
         
         return Project(**data)
     
-    def create(self, project: Project):
-        project_id = str(uuid.uuid4())
-        project.id = project_id
-
-        self.ref.child(project_id).set(project.model_dump())
-
-        return project
+    def update(self, project_id, update_data):
+        self.ref.child(project_id).update(update_data)
+        return self.get_by_id(project_id)
+    
+    def delete(self, project_id):
+        self.ref.child(project_id).delete()
+        return True
     
 class TaskRepository:
     def __init__(self):
         self.ref = firebase_db.reference("tasks")
+
+    def create(self, task: Task):
+        task_id = str(uuid.uuid4())
+        task.id = task_id
+
+        self.ref.child(task_id).set(task.model_dump())
+
+        return task
 
     def get_all(self):
         data = self.ref.get()
@@ -40,17 +56,24 @@ class TaskRepository:
         
         return [Task(**value) for value in data.values()]
     
+    def get_by_id(self, task_id):
+        data = self.ref.child(task_id).get()
+        if not data or not isinstance(data, dict):
+            return None
+        
+        return Task(**data)
+    
     def get_by_project(self, project_id):
         all = self.get_all()
         return [task for task in all if task.project_id == project_id]
     
-    def create(self, task: Task):
-        task_id = str(uuid.uuid4())
-        task.id = task_id
-
-        self.ref.child(task_id).set(task.model_dump())
-
-        return task
+    def update(self, task_id, update_data):
+        self.ref.child(task_id).update(update_data)
+        return self.get_by_id(task_id)
+    
+    def delete(self, task_id):
+        self.ref.child(task_id).delete()
+        return True
     
 project_repository = ProjectRepository()
 task_repository = TaskRepository()
