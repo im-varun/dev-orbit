c639d1e1fcb0c1e7246030c389d893c713dcca02
im-varun <vrun.mulchandani@gmail.com>
2026-02-16 18:51:04 -0600

    Add cross origin source middleware

--- server/app/main.py
+++ server/app/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes.project_routes import project_router
 from app.routes.task_routes import task_router
 
 app = FastAPI(title="Dev Orbit API")
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(project_router)
 app.include_router(task_router)
